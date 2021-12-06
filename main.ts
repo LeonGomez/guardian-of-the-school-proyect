ed6967e0cb324e341426f3466679e343e753ae82
LeonGomez <92492638+LeonGomez@users.noreply.github.com>
2021-12-05 19:55:39 -0600

    nuevo nombre

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@ controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
     Magia_verde.setStayInScreen(false)
 })
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    Hechicera.vy = -120
+    Hechicera.vy = -155
 })
 sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
     Bola_de_fuego.destroy(effects.warmRadial, 100)
@@ -19,14 +19,8 @@ let Hechicera: Sprite = null
 scene.setBackgroundImage(assets.image`myImage1`)
 Hechicera = sprites.create(assets.image`M_Atras`, SpriteKind.Player)
 controller.moveSprite(Hechicera, 100, 52)
-tiles.setTilemap(tilemap`level1`)
+tiles.setTilemap(tilemap`level2`)
 Hechicera.ay = 200
 scene.cameraFollowSprite(Hechicera)
 Hechicera.setStayInScreen(false)
 info.setLife(3)
-game.onUpdateInterval(1000, function () {
-    Bola_de_fuego = sprites.create(assets.image`Bola de fuego`, SpriteKind.Enemy)
-    Bola_de_fuego.setVelocity(0, 100)
-    Bola_de_fuego.setPosition(randint(12, 150), 0)
-    Bola_de_fuego.setStayInScreen(false)
-})
